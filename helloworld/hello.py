7565ad235d22544a0b98657278d4501fd4bf5ede
Amit <amit.jahagirdar@synerzip.com>
2019-11-11 00:16:19 -0600

    test fix4

--- helloworld/hello.py
+++ helloworld/hello.py
@@ -1,4 +1,5 @@
 
 def add(this,that):
    	result = this + that
+   	print("Hello World"+ result)
     return result
--- helloworld/test_hello.py
+++ helloworld/test_hello.py
@@ -1,4 +1,5 @@
-
+import pytest
+from hello import add
 def test_add():
     res = add(10,15)
     assert  res == 25
